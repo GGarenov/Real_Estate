99feb7bdf3b5770b48c6e99721b3986b6efe237e
Georgi <g_garenov@outlook.com>
2024-04-17 18:22:21 +0300

    So I added an useState and loading effect which I added to the button and make it disabled if the state is loading and if its loading the text is loading.. and if its not the text is sign-up

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 export default function SignUp() {
   const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const handleChange = (e) => {
     setFormData({
@@ -19,10 +21,13 @@ export default function SignUp() {
       body: JSON.stringify(formData),
     });
     const data = await res.json();
-    console.log(data);
+    if (data.success === false) {
+      setError(data.message);
+      setLoading(false);
+      return;
+    }
+    setLoading(false);
   };
-
-  console.log(formData);
 
   return (
     <div className="p-3 max-w-lg mx-auto">
@@ -49,8 +54,11 @@ export default function SignUp() {
           id="password"
           onChange={handleChange}
         />
-        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-          Sign Up
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Sign Up"}
         </button>
       </form>
       <div className="flex gap-2 mt-5">
